e9946769e9b6364a04064aa58f3e5451a67203f0
17mahera <maomaoam.aki@gmail.com>
2017-12-04 22:02:05 -0500

    DON'T SUBMIT YET Added Form and Form Responsive stuff

--- Homepage.js
+++ Homepage.js
@@ -15,3 +15,30 @@ window.onload = () => {
     broboken.style.color="white";
 });
 };
+function submitIt()
+{
+  var landmarkname= document.getElementById('landmark-name').value;
+  var landmarkreason= document.getElementById('landmark-reason').value;
+  //var = document.getElementById('').value;
+  addDirectlyToTheDamnList(landmarkname,landmarkreason);
+}
+function addDirectlyToTheDamnList(landmarkname,landmarkreason)
+{
+
+  var parentDiv= document.createElement("div");
+  var nametext= document.createElement("p");
+  nametext.innerHTML=landmarkname;
+  var linktext= document.createElement("a");
+  linktext.alt=landmarkname;
+  linktext.href="placeholder";
+  var imgtext= document.createElement("img");
+  imgtext.src="./Image-Resources/no-image.png";
+  linktext.appendChild(imgtext);
+  parentDiv.appendChild(nametext);
+  parentDiv.appendChild(linktext);
+
+  var theBigList= document.getElementById('imagelist');
+
+  theBigList.appendChild(parentDiv);
+
+}
